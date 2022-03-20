29079b8ddc141ae2e96b44fd706e16158566de23
Sanket, Wawale <sanket.wawale@gmail.com>
2022-03-20 22:02:19 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-west-2"
+}
+#creating an aws instance
+resource "aws_instance" "webserver" {
+  ami             = "ami-0341aeea105412b57"
+  instance_type   = "t2.micro"
+  subnet_id       = "subnet-8fb4d0f7"
+  key_name        = "Windows"
+  count = 1
+  security_groups = ["sg-0f30ecdf26ae644db"]
+  
+tags = {
+    Name = "computer"
+  }
+
+}
